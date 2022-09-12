e23459427b95daf3c9750d0ea5acb9f00de40599
Anastasiia Lytvyn <lytvynana@gmail.com>
2022-09-12 12:56:20 +0300

    feat: add helloWorld controller

---
+++ MySpringWebHelloWorld/src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    String helloWorld() {
+        return "Hello, World!";
+    }
+}
