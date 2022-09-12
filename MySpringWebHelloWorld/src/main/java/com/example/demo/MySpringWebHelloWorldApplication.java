b9ad6a15c3edc252cc50bea6a6413be72567676e
Anastasiia Lytvyn <lytvynana@gmail.com>
2022-09-12 12:49:21 +0300

    feat: add initial setup for web app

---
+++ MySpringWebHelloWorld/src/main/java/com/example/demo/MySpringWebHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringWebHelloWorldApplication.class, args);
+	}
+
+}
