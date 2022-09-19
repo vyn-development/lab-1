382ddef5237806e21894084e5002610caa0ae8a3
Vlad Zusko <vladzvu11@gmail.com>
2022-09-19 18:45:03 +0300

    feat: update team controller

--- MySpringWebHelloWorld/src/main/java/com/example/demo/controllers/TeamController.java
+++ MySpringWebHelloWorld/src/main/java/com/example/demo/controllers/TeamController.java
@@ -1,16 +1,41 @@
 package com.example.demo.controllers;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.entities.Person;
 
 @Controller
-public class TeamInfoController {
+@RequestMapping("/team")
+public class TeamController {
+	private HashMap<String, Person> team = new HashMap<String, Person>();
 	
-	@GetMapping("/teamInfo")
-	//@ResponseBody
-	public String teamInfo() {
-		return "C:\\Users\\User\\Spring_Workspace\\lab-1\\MySpringWebHelloWorld\\src\\main\\resources\\static\\index.html";
+	public TeamController() {
+		team.put("Yaroslav", new Person("Yaroslav", "", ""));
+		team.put("Vladyslav", new Person("Vladyslav", "", ""));
+		team.put("Anastasia", new Person("Anastasia", "", ""));
+	}
+
+	@GetMapping
+	public String getTeamPage(Model model) {
+		model.addAttribute("team", team.values().toArray());
+		
+		return "team";
+	}
+	
+	@GetMapping("/{member}")
+	public String getMemberPage(@PathVariable("member") String member, Model model) {
+		if (!team.containsKey(member)) {
+			return "redirect:/team";
+		}
+		
+		model.addAttribute("member", team.get(member));
+
+	    return "member";
 	}
 }
